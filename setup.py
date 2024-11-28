495a4feccddcb498d8ea365f5094524789546a4b
Vlad Florea <vladalexgit@users.noreply.github.com>
2024-11-28 16:32:34 +0200

    set up python package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ts2vec',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+    include_package_data=True,
+)
